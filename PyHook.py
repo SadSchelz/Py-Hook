2d4ca7eb1c1b9fc037e74909105063eb7e42a1ae
Schelz <67759165+SadSchelz@users.noreply.github.com>
2021-08-25 17:34:51 +0300

    Update PyHook.py

--- PyHook.py
+++ PyHook.py
@@ -20,63 +20,47 @@ except ImportError:
     os.system("pip install freetype-py")
 import threading
 import ctypes
-from OpenGL.GL import *
-from OpenGL.GLU import *
-from OpenGL.GL.shaders import *
-from OpenGL.GLUT import *
+try:
+    from OpenGL.GL import *
+    from OpenGL.GLU import *
+    from OpenGL.GL.shaders import *
+    from OpenGL.GLUT import *
+except ImportError:
+    os.system("pip install PyOpenGL")
 if sys.platform == "win32":
     import win32api
     import win32gui
 
 vertex_src = """
 # version 330
-layout(location = 0) in vec3 a_position;
-layout(location = 1) in vec3 a_color;
-out vec3 v_color;
+in vec4 position;
 void main()
 {
-    gl_Position = vec4(a_position, 1.0);
-    v_color = a_color;
+    gl_Position = position;
 }
 """
 
-fragment_src = """
-# version 330
-in vec3 v_color;
-out vec4 out_color;
-void main()
-{
-    out_color = vec4(v_color, 1.0);
-}
-"""
-
-def _shader(vertex_src: str, fragments_src: str, vertices: list, indices: list or None, EBO_buffer: bool):
-    shader = compileProgram(compileShader(vertex_src, GL_VERTEX_SHADER), compileShader(fragment_src, GL_FRAGMENT_SHADER))
-
-    VBO = glGenBuffers(1)
-    glBindBuffer(GL_ARRAY_BUFFER, VBO)
-    glBufferData(GL_ARRAY_BUFFER, vertices.nbytes, vertices, GL_STATIC_DRAW)
-
-    if EBO_buffer == False: pass
-    else:
-        EBO = glGenBuffers(1)
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO)
-        glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.nbytes, indices, GL_STATIC_DRAW)
-        glBufferData(GL_ELEMENT_ARRAY_BUFFER, vertices.nbytes, vertices, GL_STATIC_DRAW)
-
-    glEnableVertexAttribArray(0)
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 24, ctypes.c_void_p(0))
-
-    glEnableVertexAttribArray(1)
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 24, ctypes.c_void_p(12))
-    glUseProgram(shader)
 
 class mem_proc:
-    def __init__(self, proc_name: str):
+    def __init__(self, proc_name: str or None):
         super().__init__()
         """"No need to put .exe"""
-        self.local_pid = os.getpid()
-        self.proc_name = proc_name
+        if proc_name == 'local': self.proc_name = os.getpid()
+        if proc_name == None: pass
+        else: self.proc_name = proc_name
+
+    def GetProcessDir(processName):
+        NewprocessName = str(processName)
+        for proc in psutil.process_iter():
+            if proc.name() == NewprocessName:
+                taskid = proc.pid
+        p = psutil.Process(taskid)
+        processPath = str(p.exe())
+        NewPath = processPath.replace(processName, '')
+        return NewPath
+    
+    def LocalPath(self):
+        return os.path.dirname(os.path.realpath(__file__))
 
     def GetProcPID(self):
         pid = None
@@ -98,46 +82,50 @@ class mem_proc:
             process_id = lpdw_process_id.value
             return process_id  
 
-class Draw:
-    def __init__(self):
-        pass
+def load_shader(shader_file: str):
+    shader_source = ""
+    with open(mem_proc('local').LocalPath()+'\\shaders\\'+shader_file+'.fs') as f:
+        shader_source = f.read()
+    f.close()
+    return str.encode(shader_source)
 
+def compile_shader(color: str, vertices: list, indices: list or None, EBO_buffer: bool):
+    shader = compileProgram(compileShader(vertex_src, GL_VERTEX_SHADER), compileShader(load_shader(color), GL_FRAGMENT_SHADER))
+
+    VBO = glGenBuffers(1)
+    glBindBuffer(GL_ARRAY_BUFFER, VBO)
+    glBufferData(GL_ARRAY_BUFFER, vertices.nbytes, vertices, GL_STATIC_DRAW)
+
+    if EBO_buffer == False: pass
+    else:
+        EBO = glGenBuffers(1)
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO)
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.nbytes, indices, GL_STATIC_DRAW)
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, vertices.nbytes, vertices, GL_STATIC_DRAW)
+
+    glEnableVertexAttribArray(0)
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 24, ctypes.c_void_p(0))
+
+    glEnableVertexAttribArray(1)
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 24, ctypes.c_void_p(12))
+    glUseProgram(shader)
+
+class Draw:
     def vertex_array(self, array: list):
         return numpy.array(array, dtype=numpy.float32)
     
     def indices(self, array: list):
         return numpy.array(self.indices, dtype=numpy.uint32)
 
-    
 
     def _Text(self, window, message: str, pos_x: int, pos_y: int, color: tuple, font: str):
         pass
 
-
     def _Image(self, width: int, height: int, alpha: bool):
         pass
 
-    def _Line(self):
-        vertices = [
-            -0.5, -0.5, 0.0, 
-            0.5, -0.5, 0.0, 
-            0.0, 0.5, 0.0
-        ]
-
-        color = [
-            1.0, 0.0, 0.0,
-            0.0, 1.0, 0.0,
-            0.0, 0.0, 1.0
-        ]
-        self.vertex_array(vertices)
-        self.vertex_array(color)
-
-        glEnable(GL_VERTEX_ARRAY)
-        glVertexPointer(3, GL_FLOAT, 0, vertices)
-
     def _3D_Object(self, vertices, color):
         pass
-
 
 def window_resize(window, width: int, height: int):
     glViewport(0, 0, width, height)
@@ -173,13 +161,15 @@ class VirtualWindow:
 
         glfw.make_context_current(self._windll)
 
-        vertices = [-0.5, -0.5, 0.0, 1.0, 0.0, 0.0,
-                    0.5, -0.5, 0.0, 0.0, 1.0, 0.0,
-                    -0.5,  0.5, 0.0, 0.0, 0.0, 1.0,
-                    0.5,  0.5, 0.0, 1.0, 1.0, 1.0]
+        thickness = 1       # range 1 - 10
+
+        vertices = [-thickness/1000, -0.5, 0.0, 1.0, 0.0, 0.0,
+                    0.01, -0.5, 0.0, 0.0, 1.0, 0.0,
+                    -thickness/1000,  0.5, 0.0, 0.0, 0.0, 1.0,
+                    0.01,  0.5, 0.0, 1.0, 1.0, 1.0]
 
         vertices = Draw().vertex_array(vertices)
-        _shader(vertex_src, fragment_src, vertices, None, False)
+        compile_shader('pink', vertices, None, False)
         #self.rotation_loc = glGetUniformLocation(shader, "rotation")
 
     def DoWork(self):
